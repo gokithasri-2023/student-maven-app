5796b45ff8578931148b33891ea55e977f4be008
gokithasri-2023 <gokithasri.s2023@vitstudent.ac.in>
2026-03-09 22:37:18 +0530

    Student.java

--- src/main/java/com/example/Student.java
+++ src/main/java/com/example/Student.java
@@ -1,15 +1,58 @@
 package com.example;
 
-public class Student {
+import java.util.Scanner;
 
-    public String getGrade(int marks) {
+public class App {
 
-        if (marks >= 90) {
-            return "A";
-        } else if (marks >= 75) {
-            return "B";
-        } else {
-            return "C";
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Simple Calculator");
+        System.out.println("Enter first number:");
+        int num1 = sc.nextInt();
+
+        System.out.println("Enter second number:");
+        int num2 = sc.nextInt();
+
+        System.out.println("Choose operation:");
+        System.out.println("1. Addition");
+        System.out.println("2. Subtraction");
+        System.out.println("3. Multiplication");
+        System.out.println("4. Division");
+
+        int choice = sc.nextInt();
+
+        int result = 0;
+
+        switch(choice)
+        {
+            case 1:
+                result = num1 + num2;
+                System.out.println("Result: " + result);
+                break;
+
+            case 2:
+                result = num1 - num2;
+                System.out.println("Result: " + result);
+                break;
+
+            case 3:
+                result = num1 * num2;
+                System.out.println("Result: " + result);
+                break;
+
+            case 4:
+                if(num2 != 0)
+                    System.out.println("Result: " + (num1 / num2));
+                else
+                    System.out.println("Cannot divide by zero");
+                break;
+
+            default:
+                System.out.println("Invalid choice");
         }
+
+        sc.close();
     }
 }
